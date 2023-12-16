2ed90985aa80b2b0e11147a7c9a1e6835ee29552
kamal <kamal.batra@gmail.com>
2023-12-16 17:00:32 +0530

    first node.js deploy

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const app = express();
 const port = 9000;
 
 
+app.use("/home", (req, res) => {
+  
+  res.send("kaku chutiya");
+});
+
+app.use("/kamal", (req, res) => {
+  res.send("kamal chutiya");
+});
+
 app.use("/", (req, res) => {
   res.json({ message: "Hello From Express App" });
 });
+
 
 app.listen(9000, () => {
   console.log(`Starting Server on Port ${port}`);
